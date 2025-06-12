ffd17f1833d872ea250301b072c164703f2b133f
PupZemli-code <m.i.s.h.a.9611@mail.ru>
2025-06-12 18:43:58 +0300

    1 Написаны функции и тесты

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -10,31 +13,84 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
-	// ваш код здесь
+func generateRandomElements(size int) ([]int, error) {
+	if size <= 0 {
+		return []int{}, fmt.Errorf("invalid function argument: size %d <= 0", size)
+	}
+	var randValues []int
+	src := rand.NewSource(time.Now().Unix())
+	for i := 0; i < size; i++ {
+		randomNumber := src.Int63()
+		randomNumber = randomNumber%int64(size) + 1
+		randValues = append(randValues, int(randomNumber))
+	}
+	return randValues, nil
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) int {
-	// ваш код здесь
+func maximum(data []int) (int, error) {
+	maxValue := 0
+	if len(data) <= 1 {
+		return 0, fmt.Errorf("incorrect size slice: data = nil or 0 or 1")
+	}
+	for _, value := range data {
+		if maxValue < value {
+			maxValue = value
+		}
+	}
+	return maxValue, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
-	// ваш код здесь
+func maxChunks(data []int) (int, error) {
+	if len(data) <= 1 {
+		return 0, fmt.Errorf("incorrect size slice: data = nil or 0 or 1")
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	lenSlice := len(data) / CHUNKS
+	maxValueSlice := make([]int, 8)
+	for i := 0; i < CHUNKS; i++ {
+		slice := data[i*lenSlice : i*lenSlice+lenSlice]
+		wg.Add(1)
+		go func(s []int) {
+			defer wg.Done()
+			maxVal := 0
+			for _, val := range slice {
+				if val > maxVal {
+					maxVal = val
+				}
+			}
+			mu.Lock()
+			maxValueSlice = append(maxValueSlice, maxVal)
+			mu.Unlock()
+		}(slice)
+	}
+	wg.Wait()
+	maxVal := 0
+	for _, val := range maxValueSlice {
+		if val > maxVal {
+			maxVal = val
+		}
+	}
+	return maxVal, nil
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	slice, _ := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
-
+	start := time.Now() // начало отсчета
+	max, _ := maximum(slice)
+	elapsed := time.Since(start) // конец отсчета
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
-
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max, _ = maxChunks(slice)
+	elapsed = time.Since(start)
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Print(max)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,94 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 // Пишите тесты в этом файле
+func Test_generateRandomElements_size(t *testing.T) {
+	// проверка: длина = 0
+
+	size := 0
+	randValues, err := generateRandomElements(size)
+	require.Error(t, err)
+	require.Len(t, randValues, 0)
+
+	// проверка: длина = 1
+
+	size = 1
+	randValues, err = generateRandomElements(size)
+	require.NoError(t, err)
+	require.Len(t, randValues, 1)
+
+	// проверка: длина = 10
+
+	size = 10
+	randValues, err = generateRandomElements(size)
+	require.NoError(t, err)
+	require.Len(t, randValues, 10)
+}
+
+func Test_maximum_GiveEmptySlice(t *testing.T) {
+	// проверка: len slice = nil
+
+	var randValues []int
+	value, err := maximum(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: lin slice = 0
+
+	randValues = make([]int, 0)
+	value, err = maximum(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: len slice = 1
+
+	randValues = append(randValues, 1)
+	value, err = maximum(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: len slice = 9 (корректный запрос)
+
+	randValues = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	value, err = maximum(randValues)
+	require.NoError(t, err)
+	require.Equal(t, value, 9)
+}
+
+func Test_maxChunks_SizeSlice(t *testing.T) {
+
+	// проверка: len slice = nil
+
+	var randValues []int
+	value, err := maxChunks(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: lin slice = 0
+
+	randValues = make([]int, 0)
+	value, err = maxChunks(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: len slice = 1
+
+	randValues = append(randValues, 1)
+	value, err = maxChunks(randValues)
+	require.Error(t, err)
+	require.Equal(t, value, 0)
+
+	// проверка: len slice = 80 (корректный запрос)
+
+	slice := make([]int, 80)
+	for i := 0; i < 80; i++ {
+		slice[i] = i
+	}
+	maxVal, _ := maxChunks(slice)
+	require.Equal(t, maxVal, 79)
+}
